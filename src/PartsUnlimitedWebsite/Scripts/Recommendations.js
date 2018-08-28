65cf778fc3a157d713e8399b98f0a80e4d127130
Sriramdas Balaji <sriramdas.balaji@outlook.com>
2018-08-28 13:55:55 +0000

    Updated Recommendations.js

--- src/PartsUnlimitedWebsite/Scripts/Recommendations.js
+++ src/PartsUnlimitedWebsite/Scripts/Recommendations.js
@@ -7,7 +7,7 @@
             $("#recommendations-panel").html(data);
         },
         error: function (error) {
-            alert("Failed to get recommendations...");
+           // alert("Failed to get recommendations...");
         },
     })
 }
